c6b7b9a08ed6daf95843d994936f273564e94a0a
Daniela-Choque <dpcfjcrc@gmail.com>
2020-11-20 14:48:18 -0400

    lista actual

--- parciales/actualizar.php
+++ parciales/actualizar.php
@@ -9,6 +9,21 @@ $aprecio=$_POST['aprecio'];
 $aimagen=$_FILES['aimagen'];
 
 
+/*if($imagen["type"] == "image/jpg" OR $imagen["type"] == "image/jpeg"){
+    $dir="img/" .$_FILES["imagen"]["name"];
+    move_uploaded_file($_FILES["imagen"]["tmp_name"],$dir);
+    $sql="UPDATE producto SET nombre='$anombre', contenido='$acontenido', precio='$aprecio', imagen='$dir' WHERE id_producto='$ids'";
+    echo mysqli_query($conexion, $sql);
+
+    $ruta = "img/".md5($imagen["tmp_name"]).".jpg";
+    $sql="INSERT INTO producto(nombre, contenido, precio, imagen) VALUES('$nombre', '$contenido', '$precio', '$ruta')";
+   if(mysqli_query($conexion, $sql)){
+        move_uploaded_file($imagen["tmp_name"], $ruta);
+      
+    }     
+}*/
+
+
 $sql="UPDATE producto SET nombre='$anombre', contenido='$acontenido', precio='$aprecio', imagen='$aimagen' WHERE id_producto='$ids'";
 echo mysqli_query($conexion, $sql);
 ?>
